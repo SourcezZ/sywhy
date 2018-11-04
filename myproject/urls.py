6aa3d9939dceca780937b8b5780432c54fd6b297
heyuan <heyuan@csii.com.cn>
2018-11-04 12:15:03 +0800

    settings and urls

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
 import myapp.urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(myapp.urls)),
     url(r'^$', TemplateView.as_view(template_name="index.html")),
-]
+] + static(settings.STATIC_URL, document_root = settings.STATICFILES_DIRS)
