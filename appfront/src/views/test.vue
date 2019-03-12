5825ca280bf4840f7a61f2a5073d3ce53ad06432
heyuan <heyuan@csii.com.cn>
2019-03-12 20:34:37 +0800

    title

--- appfront/src/views/test.vue
+++ appfront/src/views/test.vue
@@ -3,6 +3,7 @@
     <span>{{ new Date() | dataFormat('yyyy-MM-dd hh:mm:ss') }}</span>
     <br>
     <span>{{ hello }}</span>
+    <span @click="aaa(2,{nihao:2, buhao:3})">haha1</span>
   </div>
 </template>
 <script>
@@ -11,17 +12,31 @@
     data() {
       return {
         hello: new Date(),
+        hehe:2
       }
     },
     watch: {
     },
     methods: {
+      aaa : function (haha1=1, {nihao = 'nihao',buhao = 'buhao'}) {
+        console.log(haha1)
+        console.log(nihao)
+        console.log(buhao)
+      },
+      aa : function(){
+        console.log(/nihao/.test('nihao'))
+        var funcName = (x) => x + 2
+      },
     },
-    mounted:function(){
-        this.hello = this.hello.Format("yyyy-MM-dd hh:mm:ss qq S")
-        this.text1()
-        func2.sayHello1()
-        func2.sayHello()
+    mounted:function (){
+    // function(){
+    //     this.hello = this.hello.Format("yyyy-MM-dd hh:mm:ss qq S")
+    //     this.text1()
+    //     func2.sayHello1()
+    //     func2.sayHello()
+    // }
+      // this.aa()
+      console.log(/nihao/.test('nihao'))
     }
   }
 </script>
