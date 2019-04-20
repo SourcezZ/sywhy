13723525cef20baaccff7a3b445695ecaec02228
heyuan <heyuan@csii.com.cn>
2019-04-20 10:40:33 +0800

    init

--- myapp/middle/loginMiddle.py
+++ myapp/middle/loginMiddle.py
@@ -1,10 +1,43 @@
 # -- coding: utf-8 --
 from django.utils.deprecation import MiddlewareMixin
+from myapp.middle.myappUtils import token as tokenUtil
+from myapp.middle.myappUtils import apiList
+import json
+from django.http import JsonResponse
 
+def getApiFlag(api):
+        ''' 不需要登陆  => 0 \n 不需登陆  => 1 \n 不在api中 => 2 \n '''
+        if api in apiList.UNNEEDLOGIN:
+            return 0
+        elif api in apiList.NEEDLOGIN:
+            return 1
+        else:
+            return 2
 
-class middle11(MiddlewareMixin):
+def userLoginInfo(token):
+    username = tokenUtil.get_username(token)
+    return username
+
+class middleLogin(MiddlewareMixin):
     def process_request(self, request):
-        print("")
+        response = {}
+        try:
+            request.data = json.loads(request.body)
+        except Exception:
+            request.data = {}
+
+        api = str(request.path).replace('/api/','')
+        apiFlag = getApiFlag(api)
+
+        #需要登陆的请求，返回'需要登陆'；非法请求返回'非法请求'
+        if apiFlag == 1:
+            username = userLoginInfo(request.data['token'])
+            if username == None:
+                response['msg'] = 'please log in'
+                return response
+        elif apiFlag == 2:
+            response['msg'] = 'illegal request'
+            return response
 
     def process_response(self, request, response):
         print("")
@@ -14,7 +47,7 @@ class middle11(MiddlewareMixin):
 class middle2(MiddlewareMixin):
     def process_request(self, request):
         print("中间件2请求")
-
+        
     def process_response(self, request, response):
         print("中间件2返回")
         return response
---
+++ myapp/middle/myappUtils/apiList.py
@@ -0,0 +1,17 @@
+NEEDLOGIN = [
+    'add_book',
+    'add_story',
+    'add_comments',
+    'uploadimg',
+]
+
+UNNEEDLOGIN = [
+    '/',
+    'show_books',
+    'show_storys',
+    'show_comments',
+    'showImg',
+    'add_user',
+    'login',
+    'userInfo',
+]
