3f3c159a71f430d680c648574ed9a9fa654faba4
heyuan <heyuan@csii.com.cn>
2019-08-21 15:49:55 +0800

    112

---
+++ myapp/middle/myappUtils/chat_with_tuling.py
@@ -0,0 +1,33 @@
+# -- coding: utf-8 --
+import requests
+import json
+from myproject.settings import TULING_API_KEY
+
+
+def chat_with_tuling(word):
+    req = {
+        "reqType": 0,
+        "perception": {
+            "inputText": {
+                "text": word
+            },
+            "selfInfo": {
+                "location": {
+                    "city": "重庆",
+                    "province": "重庆",
+                    "street": "渝北区锦橙路5号"
+                }
+            }
+        },
+        "userInfo": {
+            "apiKey": TULING_API_KEY,
+            "userId": "my_web"
+        }
+    }
+    req = json.dumps(req)
+    url = 'http://openapi.tuling123.com/openapi/api/v2'
+    response = requests.post(url, data=req)
+    content = json.loads(response.text)
+
+    # res = content['results'][0]['values']['text']
+    return content
