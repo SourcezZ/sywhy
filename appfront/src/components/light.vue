d40650d0509b30ee8ec9ea0a69bac650b13fd615
heyuan <heyuan@csii.com.cn>
2020-04-23 18:22:22 +0800

    隐藏live_2d

--- appfront/src/components/light.vue
+++ appfront/src/components/light.vue
@@ -1,17 +1,20 @@
 <!-- 走马灯组件 -->
 <template>
     <div style="text-align:center">
-        <el-carousel :interval="4000" type="card" height="100px">
-            <el-carousel-item>
-                <img class="img" src="../light/1.jpg">
-            </el-carousel-item>
+        <el-carousel :interval="4000" type="card">
+<!--            <el-carousel-item>-->
+<!--                <img class="img" src="../light/1.jpg">-->
+<!--            </el-carousel-item>-->
 
-            <el-carousel-item>
-                <img class="img" src="../light/2.jpg">
-            </el-carousel-item>
+<!--            <el-carousel-item>-->
+<!--                <img class="img" src="../light/2.jpg">-->
+<!--            </el-carousel-item>-->
 
+<!--            <el-carousel-item>-->
+<!--                <img class="img" src="../light/3.jpg">-->
+<!--            </el-carousel-item>-->
             <el-carousel-item>
-                <img class="img" src="../light/3.jpg">
+                <img class="img" src="https://api.r10086.com:8443/动漫综合2.php">
             </el-carousel-item>
         </el-carousel>
         <el-input class="tl_input" v-model="words" placeholder="和我儿子对话"/>
@@ -58,22 +61,22 @@ export default {
 .tl_input {
     width: 50%;
 }
-.el-carousel__item h3 {
-    color: #475669;
-    font-size: 14px;
-    opacity: 0.75;
-    line-height: 200px;
-    margin: 0;
-    text-align: center;
-}
+/*.el-carousel__item h3 {*/
+/*    color: #475669;*/
+/*    font-size: 14px;*/
+/*    opacity: 0.75;*/
+/*    line-height: 200px;*/
+/*    margin: 0;*/
+/*    text-align: center;*/
+/*}*/
 
-.el-carousel__item:nth-child(2n) {
-    background-color: #99a9bf;
-}
+/*.el-carousel__item:nth-child(2n) {*/
+/*    background-color: #99a9bf;*/
+/*}*/
 
-.el-carousel__item:nth-child(2n + 1) {
-    background-color: #d3dce6;
-}
+/*.el-carousel__item:nth-child(2n + 1) {*/
+/*    background-color: #d3dce6;*/
+/*}*/
 
 .img {
     width: 100%;
